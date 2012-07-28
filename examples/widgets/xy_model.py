bf161c90726b4db5aab26a252bfa824571a288c6
Kelsey Jordahl <kjordahl@enthought.com>
2012-07-28 12:48:06 -0400

    XY plot demo has adjustable frequency

--- examples/widgets/xy_model.py
+++ examples/widgets/xy_model.py
@@ -1,6 +1,6 @@
 import threading
 import numpy as np
-from traits.api import HasTraits, Array, Int, Float, Instance, on_trait_change
+from traits.api import HasTraits, Array, Int, Float, Instance, on_trait_change, Property
 import enaml
 from enaml.qt.qt_local_application import QtLocalApplication
 from enaml.noncomponents.image import ImageFromXY
@@ -8,28 +8,23 @@ from IPython.frontend.terminal.embed import InteractiveShellEmbed
 
 
 class XYModel(HasTraits):
-    N = Int(100)
+    N = Int(250)
+    omega = Float(4)
+    xmax = Float(1)
     x = Array()
     y = Array()
     # TODO: the image should really be in a plot model, not the data model
     image = Instance(ImageFromXY)
 
     def __init__(self):
+        super(XYModel, self).__init__()
         self.update_image()
 
-    @on_trait_change('N')
+    @on_trait_change('N, xmax, omega')
     def update_image(self):
-        self.x = np.linspace(0, 4 * np.pi, self.N)
-        self.y = np.sin(self.x)
+        self.x = np.linspace(0, self.xmax, self.N)
+        self.y = np.sin(2 * np.pi * self.omega * self.x)
         self.image = ImageFromXY(self.x, self.y)
-
-    def set_view(self, view):
-        self.view = view
-        container = view.children[0]
-        #self.plot = container.plot
-
-    def _N_changed(self):
-        self.update_image()
 
 
 def start_ipython(model):
@@ -41,11 +36,10 @@ def main():
         from xy_view import Main
     model = XYModel()
     view = Main(model=model)
-    model.set_view(view)
     app = QtLocalApplication()
     app.serve('main', view)
-    thread = threading.Thread(target=start_ipython, args=(model,))
-    thread.start()
+    #thread = threading.Thread(target=start_ipython, args=(model,))
+    #thread.start()
     app.mainloop()
 
 if __name__ == '__main__':
